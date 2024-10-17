01764977f0d2638de82c9b7fcfc989fc35bf0077
ajay-kr-58001 <kumarakajay1256@gmail.com>
2024-10-17 15:26:08 +0530

    add fe

--- hh.js
+++ hh.js
@@ -1,1 +1,14 @@
 // Aadd button
+
+const button = document.createElement('button');
+button.textContent = 'Add item';
+button.addEventListener('click', () => {
+  const input = document.getElementById('itemInput');
+  const list = document.getElementById('itemList');
+
+  const item = document.createElement('li');
+  item.textContent = input.value;
+
+  list.appendChild(item);
+  input.value = '';
+});
